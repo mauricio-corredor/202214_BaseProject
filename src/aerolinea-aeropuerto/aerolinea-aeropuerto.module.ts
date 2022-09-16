f0a5167368c751fcc122f01890fa86bd0b94d75f
Mauricio Corredor <malec@uniandes.edu.co>
2022-09-16 00:22:44 -0500

    Logicas aerolinea aeropuertos

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.module.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AeropuertoEntity } from '../aeropuerto/aeropuerto.entity';
 import { AerolineaEntity } from '../aerolinea/aerolinea.entity';
+import { AerolineaAeropuertoService } from './aerolinea-aeropuerto.service';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([AerolineaEntity, AeropuertoEntity])],
+  providers: [AerolineaAeropuertoService],
 })
 export class AerolineaAeropuertoModule {}
